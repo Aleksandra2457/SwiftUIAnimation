2145ab956e42af775490d6bbdc4a37e712a21e71
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-05-30 20:28:37 +0300

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Александра Лесовская on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
